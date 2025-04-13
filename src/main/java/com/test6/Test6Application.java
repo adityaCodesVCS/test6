52ebdf8f2eafd15ea6ce05b0e43f8f3dc69388a8
Aditya Tiwari <157071732+adityaCodesVCS@users.noreply.github.com>
2025-04-14 00:15:31 +0530

    Update Test6Application.java

--- src/main/java/com/test6/Test6Application.java
+++ src/main/java/com/test6/Test6Application.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test6Application {
 
 	public static void main(String[] args) {
+		String name = "Mike";
+		int x = 100;
+		boolean isPresent = false;
+		int y = 200;
+		char x = 'a';
+		int y = 200;
+		double z = 10.3;
+		boolean isPresent = false;
+		float m = 10.3f;
 		SpringApplication.run(Test6Application.class, args);
 	}
 
